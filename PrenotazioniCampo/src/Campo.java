b5a1337cd7a6c6355bd321e10274d92ce9ad98e1
Tommaso <tfava00@gmail.com>
2021-10-29 14:21:13 +0200

    Update

--- PrenotazioniCampo/src/Campo.java
+++ PrenotazioniCampo/src/Campo.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Campo {
 	
+	//array di oggetti Prenotazione
 	Prenotazione[] lista = new Prenotazione[10];
 
 	//costruttore (potrei evitare perchè uguale al default)
@@ -9,24 +10,28 @@ public class Campo {
 		
 	Scanner input = new Scanner(System.in);
 	
-	//metodo per prenotare
-	public boolean addPrenotazione() {
+	//metodo per aggiungere prenotazione, void perchè con una variabile booleana
+	//da restituire al main non sapevo come controllare se chiuso 
+	public void addPrenotazione() {
 		
-		boolean add = true;
 		System.out.print("\nInserire nome: ");
 		String nome = input.nextLine();
 		System.out.print("Inserire orario (ora esatta tra le 9 e le 18 incluse): ");
 		int orario = input.nextInt();
-		if(orario > 8 && orario < 19) {
+		if(isClosed(orario))
+			System.out.println("\nCampo chiuso!");
+		else {
 			int H = orario-9;
 			if(lista[H] != null)
-				add = false;
-			else lista[H] = new Prenotazione(nome,orario);
+				System.out.println("\nCampo occupato!");
+			else {
+				lista[H] = new Prenotazione(nome,orario);
+				System.out.println("\nPrenotazione effettuata!");
+			}
 		}
-		return add;
 	}
 	
-	//metodo per cancellare
+	//metodo per cancellare prenotazione dato nome e orario
 	public boolean removePrenotazione() {
 		 
 		boolean remove = false;
@@ -43,12 +48,25 @@ public class Campo {
 		return remove;
 	}
 	
-	//metodo per visualizzare
+	//metodo per visualizzare prenotazioni effettuate
 	public void printLista() {
 		
+		boolean check = false;
 		for(int j=0; j<10; j++) {
-			if(lista[j] != null)
+			if(lista[j] != null) {
 				System.out.println("\nOrario: " + lista[j].getOrario() + ", Nome: " + lista[j].getNome());
+				check = true;
+			}
 		}
+		if(!check)
+			System.out.println("\nNessuna prenotazione trovata!");
+	}
+	
+	public boolean isClosed(int orario) {
+		
+		boolean close = false;
+		if(orario <9 || orario > 18)
+			close = true;
+		return close;
 	}
 }
--- PrenotazioniCampo/src/gestionePrenotazione.java
+++ PrenotazioniCampo/src/gestionePrenotazione.java
@@ -5,6 +5,7 @@ public class gestionePrenotazione {
 	public static void main(String[] args) {
 		
 		Campo c = new Campo();
+		Scanner input = new Scanner(System.in);
 		
 		System.out.println("***** BENVENUTO UTENTE *****");
 		boolean avanti = true;
@@ -14,13 +15,9 @@ public class gestionePrenotazione {
 			System.out.println("2) Lista prenotazioni");
 			System.out.println("3) Esci");
 			System.out.print("\nSeleziona operazione: ");
-			Scanner input = new Scanner(System.in);
 			byte scelta = input.nextByte();
 			switch(scelta) {
-			case 0: if(c.addPrenotazione())
-				        System.out.println("\nPrenotazione effettuata con successo!");
-			        else
-			        	System.out.println("\nCampo occupato!");
+			case 0: c.addPrenotazione();
 				    break;
 			case 1: if(c.removePrenotazione())
 		        		System.out.println("\nPrenotazione cancellata con successo!");
