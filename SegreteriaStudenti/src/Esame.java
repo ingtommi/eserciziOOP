d5f33faea1c84dcc5d0515a3d5be82bd5794d8f3
MarcoCiamp <s1092617@studenti.univpm.it>
2021-11-02 16:34:21 +0100

    Update

--- SegreteriaStudenti/src/Esame.java
+++ SegreteriaStudenti/src/Esame.java
@@ -1,4 +1,49 @@
 
 public class Esame {
+	
+	private String nome;
+	private int voto;
+	private int crediti;
+	
+	//costruttore
+	
+	public Esame(String nome, int voto, int crediti) {
+		this.nome= nome;
+		this.voto= voto;
+		this.crediti=crediti;
+	}
+
+	//getter
+    public String getNome() {
+		return nome;
+	}
+
+    public int getVoto(){
+	    return voto;
+    }
+
+     public int getCrediti() {
+	return crediti;
+     }
+
+    public void setNome(String nome){
+	
+	this.nome = nome;	
+     }
+
+    public void setVoto(int voto) {
+	
+	this.voto = voto;
+    }
+
+   public void setCrediti(int crediti) {
+	
+	this.crediti = crediti;
+    }
+
+
+
+
+
 
 }
