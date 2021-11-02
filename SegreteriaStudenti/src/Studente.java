fda57c75bea7911485fcb3a0be6a137fd7b8b062
Tommaso <tfava00@gmail.com>
2021-11-02 16:32:18 +0100

    Update

--- SegreteriaStudenti/src/Studente.java
+++ SegreteriaStudenti/src/Studente.java
@@ -1,4 +1,30 @@
 
 public class Studente {
 
+	private String nome;
+	private static double matricolaUltima = 10000;
+	private double matricola;
+	//TODO: piano di studi (lista di esami)
+	
+	
+	//costruttore
+	public Studente(String nome) {
+		this.nome = nome;
+		matricola++;
+		matricola = matricolaUltima;
+		
+	}
+	
+	//getter
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getMatricola() {
+		return matricola;
+	}
+	
+	//TODO: getPiano()
+	
+	//TODO: getPiano, createPiano(), addEsame(), removeEsame(), findEsame(), printEsamiSuperati()
 }
