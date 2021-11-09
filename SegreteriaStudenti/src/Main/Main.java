65eb56df1adfd1fbb0c562175d8a86c5747b037b
Tommaso <tfava00@gmail.com>
2021-11-09 12:24:36 +0100

    Update

--- SegreteriaStudenti/src/Main/Main.java
+++ SegreteriaStudenti/src/Main/Main.java
@@ -1,10 +1,5 @@
 package Main;
 
-//importo file di altri packages (* = tutti i file)
-import Corsi.*;
-import Esami.*;
-import Persone.*;
-
 //TODO: implementare database esami e corsi di laurea
 public class Main {
 
