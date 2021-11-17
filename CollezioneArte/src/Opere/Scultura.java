3a77de707fd68f4797134fefcc09730fd4910e77
Tommaso <tfava00@gmail.com>
2021-11-17 15:02:28 +0100

    bug fix

--- CollezioneArte/src/Opere/Scultura.java
+++ CollezioneArte/src/Opere/Scultura.java
@@ -2,24 +2,24 @@ package Opere;
 
 public class Scultura extends Opera {
 	
-	private double altezza,larghezza,profondità;
+	private double altezza,larghezza,profondita;
 	
 	public Scultura(String tit, String art, double alt, double largh, double prof) {
 		super(tit,art);
 		this.altezza = alt;
 		this.larghezza = largh;
-		this.profondità = prof;
+		this.profondita = prof;
 	}
 	
 	public double getAltezza() { return altezza; }
 	public double getLarghezza() { return larghezza; }
-	public double getProfondità() { return profondità; }
+	public double getProfondita() { return profondita; }
 	public void setAltezza(double alt) { this.altezza = alt; }
 	public void setLarghezza(double largh) { this.larghezza = largh; }
-	public void setProfondità(double prof) { this.profondità = prof; }
+	public void setProfondita(double prof) { this.profondita = prof; }
 	
 	public void printingombro() {
-		double ingombro = altezza*larghezza*profondità;
+		double ingombro = altezza*larghezza*profondita;
 		System.out.println("Ingombro quadro (m^3): " + ingombro);
 	}
 }
