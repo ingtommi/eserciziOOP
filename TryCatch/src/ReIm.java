ab270507619f42d0f98c0792a2a52b15ce20ef30
MarcoCiamp <s1092617@studenti.univpm.it>
2021-11-19 17:24:43 +0100

    Update es.4

--- TryCatch/src/ReIm.java
+++ TryCatch/src/ReIm.java
@@ -1,30 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-	
+
 public class ReIm {
-	
+
 	public static void main (String[] args) throws IOException{
-    
-    Scanner input = new Scanner(System.in);
-	String s;
-	int r=0,i=0;
-	System.out.print("Inserire numero(se immaginario preceduto dalla j): ");
-	s= input.nextLine();
-    try{
-	     r=Integer.parseInt(s);
-	}catch(NumberFormatException e){
-	     if(s.charAt(0)=='j'){
-	         try{
-	              i=Integer.parseInt(s.substring(1,s.length()));          
-	            }catch(NumberFormatException x){
-	              System.out.println("impossibile");
-	            }
-	    }else System.out.println("impossibile");
-	}//fine try-catch
-	if(r!=0)System.out.println("Numero Reale:"+r);
-	if(i!=0)System.out.println("Numero Immaginario:"+i);
-	}
-	}
+
+		Scanner input = new Scanner(System.in);
+		String s;
+		double r=0,i=0;
+		System.out.print("Inserire numero reale o immaginario(immaginario preceduto dalla j): ");
+		s = input.nextLine();
+		boolean iNeg = false;
+		try{
+			r = Double.parseDouble(s);
+		}catch(NumberFormatException e){
+			s.replaceAll("[+]", "+");
+			s.replaceAll("[-]", "-");
+			if(s.contains("+j")){
+				String[] n = s.split("\\+j");
+				r = Double.parseDouble(n[0]);
+				i = Double.parseDouble(n[1]);
+			}
+			else if (s.contains("-j")) {
+				String[] n = s.split("-j");
+				r = Double.parseDouble(n[0]);
+				i = Double.parseDouble(n[1]);
+				iNeg = true;
+			}
+			else if (s.contains("j")) {
+				r=0;
+				i=Double.parseDouble(s.substring(1,s.length())); 
+			}
+			else System.out.println("impossibile");
+		}//fine try-catch
+		if(r!=0)System.out.println("Numero Reale:"+r);
+		if(iNeg)System.out.println("Numero Immaginario:-"+i);
+		else System.out.println("Numero Immaginario:"+i);
+		input.close();
+	}}
+
 
 
