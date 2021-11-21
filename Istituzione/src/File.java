438e0ec08b256612515d871cad94ddb41caba420
Tommaso <tfava00@gmail.com>
2021-11-21 16:24:52 +0100

    Added login() method

--- Istituzione/src/File.java
+++ Istituzione/src/File.java
@@ -14,10 +14,29 @@ public class File {
 			}
 			file_output.close();
 			} catch (IOException e) {
-				System.out.println("ERRORE di I/O");
-				System.out.println(e);
+				System.out.println("ERRORE di I/O: " + e);
 				ok = false;
 			}
 		return ok;
+	}
+	
+	public boolean login(int password) {
+		
+		Vector<Integer> pwd = new Vector<Integer>();
+		boolean find = false;
+		try {
+			Scanner file_input = new Scanner(new BufferedReader(new FileReader("pwd.txt")));
+			while(file_input.hasNext()) {
+				pwd.add(file_input.nextInt());
+			}
+			file_input.close();
+		} catch(IOException e) {
+			System.out.println("ERRORE DI I/O: " + e);
+		}
+		for(Integer pass : pwd) {
+			if(password == pass)
+				find = true;
+		}
+		return find;
 	}
 }
