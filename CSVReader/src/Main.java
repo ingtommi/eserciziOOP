e76a7a000d0f82fed087374b2f6dcf50a528e6e9
Tommaso <tfava00@gmail.com>
2021-11-23 20:14:00 +0100

    version 1.1

--- CSVReader/src/Main.java
+++ CSVReader/src/Main.java
@@ -5,23 +5,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<List<String>> struct = new ArrayList<>();
+		Vector<String[]> struct = new Vector<String[]>();
 		
 		try {
 			Scanner file_input = new Scanner(new BufferedReader(new FileReader("file.csv")));
 			while(file_input.hasNext()) {
 				String line = file_input.nextLine();
 				String[] tokens = line.split(",");
-				struct.add(Arrays.asList(tokens));
+				struct.add(tokens);
 			}
 			file_input.close();
 		} catch(FileNotFoundException e) {
 			System.out.println("\nFILE NON TROVATO!");
 		}
-		System.out.println("LETTURA FILE:\n");
-		for(List<String> line: struct) {
-			System.out.println("Nome: " + line.get(0) + " | Tipo: " + line.get(1) + 
-					" | Città: " + line.get(2) + " | Dimensione: " + line.get(3));
+		System.out.println("LETTURA FILE: file.csv\n");
+		for(String[] line : struct) {
+			System.out.println("Nome: " + line[0] + " | Tipo: " + line[1] + 
+					" | Città: " + line[2] + " | Dimensione: " + line[3]);
         }
 	}
 }
