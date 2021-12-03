fadbb073fd93d6f61583c45f046436b56cc5a48b
Tommaso <tfava00@gmail.com>
2021-12-03 18:02:41 +0100

    tested @RequestBody

--- HelloWorldApp/src/main/java/it/univpm/HelloWorldApp/controller/SimpleController.java
+++ HelloWorldApp/src/main/java/it/univpm/HelloWorldApp/controller/SimpleController.java
@@ -7,16 +7,20 @@ import org.springframework.web.bind.annotation.*;
 public class SimpleController {  
 	
 	//utilizzando il defaultValue posso omettere il parametro o scriverlo male (equivale ad ometterlo)
-		
 	@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)  
 	public String hello(@RequestParam(name = "age", defaultValue = "18") int age, @PathVariable("name") String name) {
 		return "Hello " + name + ", you've got " + age + " years"; 
 	}
 	
 	//in hello2() non inserisco defaultValue del parametro e quindi se lo inserisco in maniera errata ho una eccezione
-	
-	@RequestMapping(value = "/hello2/{name}", method = RequestMethod.GET)  
+	@GetMapping("/hello2/{name}")  
 	public String hello2(@RequestParam(name = "age") int age, @PathVariable("name") String name) {
 		return "Hello " + name + ", you're " + age + " year old"; 
+	}
+	
+	//viene chiesta una stringa qualsiasi nel body altrimenti c'è bad request
+	@GetMapping("/test")
+	public String test(@RequestBody String any) {
+		return "Test example!";
 	}
 }  
